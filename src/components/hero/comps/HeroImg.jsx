b51efd31be1a3e08a6e5ff746b0e3a201c4c2638
eduarf <fraude_f@yahoo.com>
2023-12-03 23:57:18 +0300

    heroImg grid

--- src/components/hero/comps/HeroImg.jsx
+++ src/components/hero/comps/HeroImg.jsx
@@ -3,6 +3,9 @@ import styled from "styled-components"
 const HeroImgStyled = styled.div`
   background-color: pink;
   flex-basis: 45%;
+  display: grid;
+  grid-template-columns: repeat(8, minmax(0, 1fr));
+  grid-template-rows: repeat(8, minmax(0, 1fr));
 `;
 
 export default function HeroImg() {
