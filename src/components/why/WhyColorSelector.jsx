7bf35349fc56be4373664aa7a7fc593ca0dd1685
eduarf <fraude_f@yahoo.com>
2024-01-09 23:06:56 +0300

    WhyColorSelector section styling

--- src/components/why/WhyColorSelector.jsx
+++ src/components/why/WhyColorSelector.jsx
@@ -6,18 +6,65 @@ import { nanoid } from "nanoid";
 
 const StyledWhyColorSelector = styled.div`
       padding: 10vh 8vw;
+      margin-bottom: 50rem;
 `;
 const StyledHeading = styled.h2`
     font-size: 6rem;
     font-weight: 700;
     letter-spacing: -.5px;
+    text-align: center;
+    margin-bottom: 4rem;
 `;
 
-const StyledCardContainer = styled.div``;
-const StyledCard = styled.div``;
-const StyledIcon = styled.div``;
-const StyledHeader = styled.h4``;
-const StyledPara = styled.p``;
+const StyledCardContainer = styled.div`
+  display: flex;
+  gap: 3rem;
+`;
+const StyledHeader = styled.h4`
+  font-size: 2.8rem;
+  position: relative;
+  z-index: 10;
+  &::before {
+    content: '';
+    position: absolute;
+    height: 10px;
+    width: 100%;
+    left: 0;
+    bottom: 0;
+    background-color: var(--color-primary);
+    transition: all .3s ease-in-out;
+    z-index: 1;
+    opacity: .5;
+  }
+`;
+const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 4rem;
+  gap: 20px;
+  background-color: var(--color-secondary);
+  border-radius: 1.5rem;
+  position: relative; 
+
+  &:hover {
+    ${StyledHeader} {
+      &::before {
+        height: 20px;
+      }
+    }
+  }
+`;
+const StyledIcon = styled.div`
+  font-size: 10rem;
+  color: var(--color-primary);
+`;
+const StyledPara = styled.p`
+  font-size: 1.8rem;
+  line-height: 1.8;
+  letter-spacing: .9px;
+`;
 
 export default function WhyColorSelector() {
 
@@ -40,7 +87,7 @@ export default function WhyColorSelector() {
   ];
   return (
     <StyledWhyColorSelector>
-      <StyledHeading>Why Realtime Colors?</StyledHeading>
+      <StyledHeading>Why Color Selector?</StyledHeading>
       <StyledCardContainer>
         {data.map((item) => {
           return <StyledCard key={nanoid()}>
