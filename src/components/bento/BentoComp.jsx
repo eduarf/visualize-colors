16a8c5f6844057c02fa7759a0e86238f55bfb5cf
eduarf <fraude_f@yahoo.com>
2024-01-16 20:58:24 +0300

    created bentocomp

---
+++ src/components/bento/BentoComp.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const StyledBentoComp = styled.div``;
+
+export default function BentoComp() {
+  return (
+    <StyledBentoComp>
+      Bento
+    </StyledBentoComp>
+  )
+}
