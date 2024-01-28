fd8f4396e1a39a31d633ff6d8c1bbad1fbe8d802
eduarf <fraude_f@yahoo.com>
2024-01-28 17:14:42 +0300

    Bentocomp Responsive

--- src/components/bento/BentoComp.jsx
+++ src/components/bento/BentoComp.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import BentoLink from "./components/BentoLink";
+import { useEffect, useState } from "react";
 
 const StyledBentoComp = styled.div`
   padding: 6vh 8vw;
@@ -8,8 +9,11 @@ const StyledBentoComp = styled.div`
   grid-template-columns: repeat(5, 1fr);
   grid-template-rows: 60% 40%;
   max-width: 1920px;
-  margin-bottom: 50rem;
   height: 70rem;
+  @media only screen and (max-width: 1000px){
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+    grid-template-rows: none;
+  }
 `;
 
 const StyledUsersCard = styled.div`
@@ -22,11 +26,24 @@ const StyledUsersCard = styled.div`
   justify-content: center;
   gap: 2rem;
   border-radius: 2rem;
+  @media only screen and (max-width: 1000px){
+    grid-column: 1 / 4;
+  }
+  @media only screen and (max-width: 768px) {
+      padding: 4rem 1rem;
+    }
   h2 {
     font-size: 6rem;
     font-weight: 800;
     letter-spacing: -1px;
     color: var(--color-background);
+    text-align: center;
+    @media only screen and (max-width: 1200px) {
+      font-size: 5.2rem;
+    }
+    @media only screen and (max-width: 768px) {
+      font-size: 3.8rem;
+    }
   }
   span {
     font-size: 1.8rem;
@@ -43,10 +60,24 @@ const StyledFreeCard = styled.div`
   align-items: center;
   border-radius: 2rem;
   gap: 2rem;
+  @media only screen and (max-width: 1000px){
+    grid-column: 1 / 4;
+  }
+  @media only screen and (max-width: 768px) {
+      padding: 4rem 1rem;
+    }
   h2 {
     font-size: 4rem;
     font-weight: 800;
     line-height: 1.2;
+    text-align: center;
+    white-space: pre-wrap;
+    @media only screen and (max-width: 1200px) {
+      font-size: 3.4rem;
+    }
+    @media only screen and (max-width: 768px) {
+      font-size: 2.6rem;
+    }
   }
   span {
     font-size: 1.8rem;
@@ -62,11 +93,24 @@ const StyledProductHuntCard = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 1rem;
+  @media only screen and (max-width: 1000px){
+    grid-column: 1 / 4;
+  }
+  @media only screen and (max-width: 768px) {
+      padding: 4rem 1rem;
+    }
   h2 {
     font-size: 3.8rem;
     letter-spacing: -.5px;
     font-weight: 600;
     color: var(--color-background);
+    text-align: center;
+    @media only screen and (max-width: 1200px){
+      font-size: 3.2rem;
+    }
+    @media only screen and (max-width: 768px) {
+      font-size: 2.6rem;
+    }
   }
 `;
 
@@ -79,15 +123,47 @@ const StyledPluginCard = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 1rem;
+  @media only screen and (max-width: 1000px){
+    grid-column: 1 / 4;
+  }
+  @media only screen and (max-width: 768px) {
+      padding: 4rem 1rem;
+    }
   h2 {
     font-size: 3.8rem;
     letter-spacing: -.5px;
     font-weight: 600;
     color: var(--color-background);
+    @media only screen and (max-width: 1200px){
+      font-size: 3.2rem;
+      text-align: center;
+    }
+    @media only screen and (max-width: 768px) {
+      font-size: 2.6rem;
+    }
   }
 `;
 
 export default function BentoComp() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  const checkScreenWidth = () => {
+    const screenWidth = window.innerWidth;
+    setIsSmallScreen(screenWidth < 1000);
+  };
+
+  useEffect(() => {
+    // Sayfa yüklendiğinde ve pencere boyutu değiştiğinde kontrol et
+    checkScreenWidth();
+
+    // Pencere boyutu değiştiğinde tekrar kontrol etmek için event listener ekle
+    window.addEventListener('resize', checkScreenWidth);
+
+    // Component unmount edildiğinde event listener'ı temizle
+    return () => {
+      window.removeEventListener('resize', checkScreenWidth);
+    };
+  }, []);
   return (
     <StyledBentoComp>
       <StyledUsersCard>
@@ -96,7 +172,7 @@ export default function BentoComp() {
       </StyledUsersCard>
       <StyledFreeCard>
         <h2>
-          100%<br></br>Free!
+          100%{isSmallScreen ? null : <br /> }Free!
         </h2>
         <span>Forever.</span>
       </StyledFreeCard>
