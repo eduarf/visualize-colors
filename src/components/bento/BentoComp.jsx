e45831bd4e288b9b80db05cf68619f828695079b
eduarf <fraude_f@yahoo.com>
2024-02-07 23:24:55 +0300

    BentoComp update

--- src/components/bento/BentoComp.jsx
+++ src/components/bento/BentoComp.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import BentoLink from "./components/BentoLink";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { isColorDark } from "../utils/helpers";
 
 const StyledBentoComp = styled.div`
   padding: 6vh 8vw;
@@ -10,7 +12,7 @@ const StyledBentoComp = styled.div`
   grid-template-rows: 60% 40%;
   max-width: 1920px;
   min-height: 70rem;
-  @media only screen and (max-width: 1000px){
+  @media only screen and (max-width: 1000px) {
     grid-template-columns: repeat(1, minmax(0, 1fr));
     grid-template-rows: none;
   }
@@ -26,17 +28,27 @@ const StyledUsersCard = styled.div`
   justify-content: center;
   gap: 2rem;
   border-radius: 2rem;
-  @media only screen and (max-width: 1000px){
+  @media only screen and (max-width: 1000px) {
     grid-column: 1 / 4;
   }
   @media only screen and (max-width: 768px) {
-      padding: 4rem 1rem;
-    }
+    padding: 4rem 1rem;
+  }
   h2 {
     font-size: 6rem;
     font-weight: 800;
     letter-spacing: -1px;
-    color: var(--color-background);
+    color: ${(props) => {
+      if (props.$isDark && props.$isThemeDark) {
+        return "#fff";
+      } else if (props.$isDark && !props.$isThemeDark) {
+        return "#fff";
+      } else if (!props.$isDark && props.$isThemeDark) {
+        return "#000";
+      } else {
+        return "#000";
+      }
+    }};
     text-align: center;
     @media only screen and (max-width: 1200px) {
       font-size: 5.2rem;
@@ -47,7 +59,17 @@ const StyledUsersCard = styled.div`
   }
   span {
     font-size: 1.8rem;
-    color: var(--color-background);
+    color: ${(props) => {
+      if (props.$isDark && props.$isThemeDark) {
+        return "#fff";
+      } else if (props.$isDark && !props.$isThemeDark) {
+        return "#fff";
+      } else if (!props.$isDark && props.$isThemeDark) {
+        return "#000";
+      } else {
+        return "#000";
+      }
+    }};
   }
 `;
 const StyledFreeCard = styled.div`
@@ -60,18 +82,29 @@ const StyledFreeCard = styled.div`
   align-items: center;
   border-radius: 2rem;
   gap: 2rem;
-  @media only screen and (max-width: 1000px){
+  @media only screen and (max-width: 1000px) {
     grid-column: 1 / 4;
   }
   @media only screen and (max-width: 768px) {
-      padding: 4rem 1rem;
-    }
+    padding: 4rem 1rem;
+  }
   h2 {
     font-size: 4rem;
     font-weight: 800;
     line-height: 1.2;
     text-align: center;
     white-space: pre-wrap;
+    color: ${(props) => {
+      if (props.$isDark && props.$isThemeDark) {
+        return "#fff";
+      } else if (props.$isDark && !props.$isThemeDark) {
+        return "#fff";
+      } else if (!props.$isDark && props.$isThemeDark) {
+        return "#000";
+      } else {
+        return "#000";
+      }
+    }};
     @media only screen and (max-width: 1200px) {
       font-size: 3.4rem;
     }
@@ -81,6 +114,17 @@ const StyledFreeCard = styled.div`
   }
   span {
     font-size: 1.8rem;
+    color: ${(props) => {
+    if (props.$isDark && props.$isThemeDark) {
+      return "#fff";
+    } else if (props.$isDark && !props.$isThemeDark) {
+      return "#fff";
+    } else if (!props.$isDark && props.$isThemeDark) {
+      return "#000";
+    } else {
+      return "#000";
+    }
+  }};
   }
 `;
 
@@ -93,19 +137,19 @@ const StyledProductHuntCard = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 1rem;
-  @media only screen and (max-width: 1000px){
+  @media only screen and (max-width: 1000px) {
     grid-column: 1 / 4;
   }
   @media only screen and (max-width: 768px) {
-      padding: 4rem 1rem;
-    }
+    padding: 4rem 1rem;
+  }
   h2 {
     font-size: 3.8rem;
-    letter-spacing: -.5px;
+    letter-spacing: -0.5px;
     font-weight: 600;
     color: var(--color-background);
     text-align: center;
-    @media only screen and (max-width: 1200px){
+    @media only screen and (max-width: 1200px) {
       font-size: 3.2rem;
     }
     @media only screen and (max-width: 768px) {
@@ -123,18 +167,18 @@ const StyledPluginCard = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 1rem;
-  @media only screen and (max-width: 1000px){
+  @media only screen and (max-width: 1000px) {
     grid-column: 1 / 4;
   }
   @media only screen and (max-width: 768px) {
-      padding: 4rem 1rem;
-    }
+    padding: 4rem 1rem;
+  }
   h2 {
     font-size: 3.8rem;
-    letter-spacing: -.5px;
+    letter-spacing: -0.5px;
     font-weight: 600;
     color: var(--color-background);
-    @media only screen and (max-width: 1200px){
+    @media only screen and (max-width: 1200px) {
       font-size: 3.2rem;
       text-align: center;
     }
@@ -146,6 +190,11 @@ const StyledPluginCard = styled.div`
 
 export default function BentoComp() {
   const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const primaryComp = useSelector((state) => state.theme.primaryComplement);
+  const secondaryColor = useSelector((state) => state.theme.secondaryColor);
+  const isThemeDark = useSelector((state) => state.theme.isDark);
+  const isPrimaryCompDark = isColorDark(primaryComp);
+  const isSecondaryDark = isColorDark(secondaryColor);
 
   const checkScreenWidth = () => {
     const screenWidth = window.innerWidth;
@@ -153,38 +202,30 @@ export default function BentoComp() {
   };
 
   useEffect(() => {
-    // Sayfa yüklendiğinde ve pencere boyutu değiştiğinde kontrol et
     checkScreenWidth();
+    window.addEventListener("resize", checkScreenWidth);
 
-    // Pencere boyutu değiştiğinde tekrar kontrol etmek için event listener ekle
-    window.addEventListener('resize', checkScreenWidth);
-
-    // Component unmount edildiğinde event listener'ı temizle
     return () => {
-      window.removeEventListener('resize', checkScreenWidth);
+      window.removeEventListener("resize", checkScreenWidth);
     };
   }, []);
   return (
     <StyledBentoComp>
-      <StyledUsersCard>
+      <StyledUsersCard $isDark={isPrimaryCompDark} $isThemeDark={isThemeDark}>
         <h2>200K+ Users</h2>
         <span>and counting...</span>
       </StyledUsersCard>
-      <StyledFreeCard>
-        <h2>
-          100%{isSmallScreen ? null : <br /> }Free!
-        </h2>
+      <StyledFreeCard $isDark={isSecondaryDark} $isThemeDark={isThemeDark}>
+        <h2>100%{isSmallScreen ? null : <br />}Free!</h2>
         <span>Forever.</span>
       </StyledFreeCard>
       <StyledPluginCard>
         <h2>11K+ Plugin Users</h2>
-        <BentoLink link='Check it out on Figma' />
+        <BentoLink link="Check it out on Figma" />
       </StyledPluginCard>
       <StyledProductHuntCard>
-        <h2>
-          200+ ProductHunt Upvotes
-        </h2>
-        <BentoLink link='Leave a review' />
+        <h2>200+ ProductHunt Upvotes</h2>
+        <BentoLink link="Leave a review" />
       </StyledProductHuntCard>
     </StyledBentoComp>
   );
